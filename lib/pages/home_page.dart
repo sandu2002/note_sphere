f53249a74ed0c64e540313bb24ec178badf8a1f9
sandu2002 <kavindusandupama.persanol@gmail.com>
2025-03-13 01:31:10 +0530

    Add Notes and To-Do List cards to HomePage

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:note_sphere/utils/constants.dart';
 import 'package:note_sphere/utils/text_styles.dart';
+import 'package:note_sphere/widgets/note_to_do_card.dart';
 import 'package:note_sphere/widgets/progress_card.dart';
 
 class HomePage extends StatefulWidget {
@@ -23,10 +25,41 @@ class _HomePageState extends State<HomePage> {
         padding: const EdgeInsets.all(10),
         child: Column(
           children: [
+            SizedBox(height: AppConstants.kDefaultPadding),
             ProgressCard(
               completedTasks: 5,
               totalTasks: 5,
-            )
+            ),
+            SizedBox(height: AppConstants.kDefaultPadding),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                NotesTodoCard(
+                  title: 'Notes',
+                  description: "3 Notes",
+                  icon: Icons.bookmark_add_outlined,
+                ),
+                NotesTodoCard(
+                  title: 'To-Do List',
+                  description: "3 Tasks",
+                  icon: Icons.today_outlined,
+                ),
+              ],
+            ),
+            const SizedBox(height: 30),
+            const Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  "Today's Progress",
+                  style: AppTextStyles.appSubtitle,
+                ),
+                Text(
+                  "See All",
+                  style: AppTextStyles.appButton,
+                ),
+              ],
+            ),
           ],
         ),
       ),
